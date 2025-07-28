1682184dee936d87febec860760646a32e0a9486
Moazzam Shaikh <moazzamshaikh8982@gmail.com>
2025-07-28 20:33:07 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,11 @@
+import os
 
+async def save_temp_file(client, message):
+    file_path = f"temp_{message.from_user.id}.jpg"
+    await client.download_media(message, file_path)
+    return file_path
+
+def cleanup_files(*paths):
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
